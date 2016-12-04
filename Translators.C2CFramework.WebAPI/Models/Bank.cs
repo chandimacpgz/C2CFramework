b01b50153bdf2dad2d22e5a9cf4f156a9a575c13
Chandima Gunawardhana <chandimacpgz@hotmail.com>
2016-12-04 22:51:40 +0530

    Initialize WebAPI

---
+++ Translators.C2CFramework.WebAPI/Models/Bank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Translators.C2CFramework.WebAPI.Models
+{
+    public class Bank
+    {
+    }
+}
