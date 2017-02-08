01730abfae7f29a535d56782569f7b3b097fcf1f
Chandima <Chandima@NINJA>
2017-02-08 16:22:31 +0530

    fix dependency in angular material

--- Translators.C2CFramework.App/app.js
+++ Translators.C2CFramework.App/app.js
@@ -1,1 +1,27 @@
-﻿
+﻿var app = angular.module('chequeApp', [
+  'ui.router', 'ngMaterial', 'ngMessages', 'cfp.loadingBar'
+]);
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+      .primaryPalette('indigo', {
+          'default': '400',
+          'hue-1': '100',
+          'hue-2': '600',
+          'hue-3': 'A100'
+      })
+      .accentPalette('purple', {
+          'default': '200'
+      });
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/dashboard");
+    $stateProvider
+        .state('dashboard',
+        {
+            url: "/dashboard",
+            templateUrl: 'views/dashboard.html',
+            controller: 'dashboardController'
+        })
+});
--- Translators.C2CFramework.App/controllers/dashboardController.js
+++ Translators.C2CFramework.App/controllers/dashboardController.js
@@ -1,1 +1,37 @@
-﻿
+﻿(function () {
+    'use strict';
+    angular
+        .module('chequeApp')
+        .controller('dashboardController', leaveDashboardController);
+
+    leaveDashboardController.$inject = ['$scope', '$state'];
+    function leaveDashboardController($scope, $state) {
+
+        $scope.goToLeave = function () {
+            $state.go('activities');
+        }
+
+        $scope.goToLeaveForm = function () {
+            $state.go('leaveForm');
+        }
+
+        $scope.goToBalances = function () {
+            $state.go('balances');
+        }
+        $scope.goToIndexProfile = function () {
+            $state.go('leaveIndexProfile');
+        }
+        $scope.goToRules = function () {
+            $state.go('rules');
+        }
+
+        $scope.goToSetApprovers = function () {
+            $state.go('getgroups');
+        }
+        $scope.goToLeaveRequest = function () {
+            $state.go('leaveRequest');
+        }
+
+    }
+
+})();
